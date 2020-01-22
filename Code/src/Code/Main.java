6a6cdceabe4959433306af34d336c83e1196487a
Aurelien Sebe <aureliensebe@gmail.com>
2020-01-22 11:30:22 +0100

    Premier bout de code

--- Code/src/Code/Main.java
+++ Code/src/Code/Main.java
@@ -1,1 +1,1 @@
-package Code;public class Main {    public static void main(String[] args){        System.out.println("Le projet est paré à décoller");    }}
+package Code;/*Crée par Aurélien SèbeVersion: 0.1.0 */public class Main {    public static void main(String[] args){        System.out.println("Le projet est paré à décoller");    }}
