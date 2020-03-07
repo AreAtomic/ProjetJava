3aa4e048fefffdf11efc3b707627450bf0226733
Camille <ca.carbasse@orange.fr>
2020-03-07 14:37:52 +0100

    modification exception entreprise modification methode calcul salaire de commerciale avec ajout de la saisie du velume mensuel de vente mise en commentaire d'une partie de la class responsable pour le ofnctionnement de la class entreprise

--- Code/src/Code/Entreprise.java
+++ Code/src/Code/Entreprise.java
@@ -9,16 +9,15 @@ import java.util.TreeMap;
 public class Entreprise {
     protected TreeMap<String, Employe> entreprise;
     protected ArrayList<String> idEmployes;
-    protected ArrayList<Integer> verifMatricules;
+    protected ArrayList<Employe> verifMatricules;
 
-    public Entreprise() throws EntrepriseException {
+    public Entreprise() throws EntrepriseException, FileNotFoundException {
         this.entreprise = new TreeMap<String, Employe>();
         this.idEmployes = new ArrayList<String>();
         this.verifMatricules = new ArrayList<>();
 
         Scanner sc;
-        try {
-            sc = new Scanner(new File("C:\\Users\\cacar\\Documents\\ProjetJava\\Code\\src\\Code\\Entreprise.txt"));
+            sc = new Scanner(new File("C:\\Users\\cacar\\Documents\\ProjetJavaVrai\\Code\\Entreprise.csv"));
             String ligne = sc.nextLine();
             while (sc.hasNext()) {
                 ligne = sc.nextLine();
@@ -26,41 +25,39 @@ public class Entreprise {
                 String id = stoken[0];
                 String nom = stoken[1];
                 String prenom = stoken[2];
-                int matricule = Integer.valueOf(stoken[3]);
-                float indice = Integer.valueOf(stoken[4]);
+                int matricule = Integer.parseInt(stoken[3]);
+                float indice = Integer.parseInt(stoken[4]);
                 this.idEmployes.add(id);
 
                 if (id.charAt(id.length() - 2) == ('E')) {
                     EmployeDeBase employe = new EmployeDeBase(nom, prenom, matricule, indice);
                     this.entreprise.put(id, employe);
-                    this.verifMatricules.add(matricule);
-                    for (int e : verifMatricules) {
-                        if (e == matricule) {
+                    this.verifMatricules.add(employe);
+                    for (Employe e : verifMatricules) {
+                        if (e.matricule == matricule && !(e.nom.equals(nom))) {
                             throw new EntrepriseException("Le matricule existe déjà: " + employe);
                         }
                     }
                 } else if (id.charAt(id.length() - 2) == ('C')) {
                     Commercial commercial = new Commercial(nom, prenom, matricule, indice);
                     this.entreprise.put(id, commercial);
-                    this.verifMatricules.add(matricule);
-                    for (int e : verifMatricules) {
-                        if (e == matricule) {
+                    this.verifMatricules.add(commercial);
+                    for (Employe e : verifMatricules) {
+                        if (e.matricule == matricule && !(e.nom.equals(nom))){
                             throw new EntrepriseException("Le matricule existe déjà: " + commercial);
                         }
                     }
                 } else {
                     Responsable respo = new Responsable(nom, prenom, matricule, indice);
                     this.entreprise.put(id, respo);
-                    this.verifMatricules.add(matricule);
-                    for (int e : verifMatricules) {
-                        if (e == matricule) {
+                    this.verifMatricules.add(respo);
+                    for (Employe e : verifMatricules) {
+                        if (e.matricule == matricule && !(e.nom.equals(nom))) {
                             throw new EntrepriseException("Le matricule existe déjà: " + respo);
                         }
                     }
                 }
-            } catch(FileNotFoundException e){
-                System.out.println(e);
-            }
+
         }
 
     }
